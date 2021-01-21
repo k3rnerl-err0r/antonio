5c87bee42163bd791ed902fcbbec9561508b015d
Antonio Quintero-Felizzola <antonio.qfel@gmail.com>
2021-01-20 21:45:46 -0500

    Added index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+console.log(`
+Antonio Quintero-Felizzola
+
+I am a passionate Web Developer in pursuit of JavaScript mastery, 
+deep curiosity in existing and emerging web technologies. 
+I'm a strong team player with a strong problem-solving addiction.
+
+🥷  TypeScript/JavaScript Ninja
+🐧 Linux Enthusiast
+🎮 Game Developer
+
+🐦 Twitter: https://twitter.com/k3rnel_err0r
+📖 GitHub: https://github.com/k3rnerl-err0r
+
+`);
