a1d69bc5b0c6ca14a57af8d64e01f548c5e91050
Antonio Quintero-Felizzola <antonio.qfel@gmail.com>
2021-03-10 10:41:40 -0500

    Added ora module

--- utils/stats.js
+++ utils/stats.js
@@ -1,14 +1,21 @@
 const axios = require('axios');
 const handleError = require('cli-handle-error');
 const to = require('await-to-js').default;
+const ora = require('ora');
+const { green, yellow } = require('chalk');
 
+const spinner = ora({ text: '' });
 const apiURL = `https://api.github.com/users/k3rnel-err0r`;
 
 module.exports = async () => {
+    spinner.start(`${green(`FOLLOWERS`)} fetching...`);
     const [error, response] = await to(axios.get(apiURL));
     handleError(`API CALLED FAILED`, error, true, true);
 
     const githubFollowers = response.data.followers;
+    // spinner.succeed(`${yellow(`FOLLOWERS`)} fetched`);
+    spinner.stop();
+    
     console.log(`
 GitHub Followers: ${githubFollowers}
 `);
